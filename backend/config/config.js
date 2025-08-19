5b78b0d4c0df6ff9a7366a6366fbe44a30c0e650
Mirko Alvarez <mirko.bolivar2003@gmail.com>
2025-08-19 12:55:13 -0300

    cambio puerto DB para Render
    Cambios en config y api para hacer deploy en render, y crecion del archivo render.yaml

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     port: process.env.PORT || 3001,
     database: {
         dialect: 'sqlite',
-        storage: './database.sqlite',
+        storage: process.env.SQLITE_PATH || './database.sqlite',
         logging: false, // Set to true to see SQL queries in console
     },
     // Otras configuraciones globales si son necesarias
